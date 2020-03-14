85a0098021b70de7fbde58ecfefa89caefb17e3e
Gaurav Ghongde <7gauravghongde@gmail.com>
2020-03-14 21:13:08 +0530

    [patch] fixed build failure

--- src/main/java/com/rstack/devnet/config/SecurityConfigurer.java
+++ src/main/java/com/rstack/devnet/config/SecurityConfigurer.java
@@ -5,6 +5,7 @@ import com.rstack.devnet.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
