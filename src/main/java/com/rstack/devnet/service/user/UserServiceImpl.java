fcab5f82d692224c56b7f90427122eb79a9f0b4a
Gaurav Ghongde <7gauravghongde@gmail.com>
2020-05-16 13:44:13 +0530

    [patch] userService added @service

--- src/main/java/com/rstack/devnet/service/user/UserServiceImpl.java
+++ src/main/java/com/rstack/devnet/service/user/UserServiceImpl.java
@@ -3,7 +3,9 @@ package com.rstack.devnet.service.user;
 import com.rstack.devnet.dto.model.UserDTO;
 import com.rstack.devnet.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
