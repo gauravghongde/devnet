de3a5f46ec4cc698d95a038f20c764c4c04d0af9
Gaurav Ghongde <7gauravghongde@gmail.com>
2021-03-21 19:02:50 +0530

    [patch] auth cred verification enabled (#17)
    * added env variable for mongo creds (#15)
    
    * Update README.md
    
    * added env variable for mongo creds
    
    * [patch] auth cred verification enabled

--- src/main/java/com/rstack/devnet/security/JwtTokenProvider.java
+++ src/main/java/com/rstack/devnet/security/JwtTokenProvider.java
@@ -1,10 +1,9 @@
 package com.rstack.devnet.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -14,6 +13,9 @@ import java.util.function.Function;
 
 @Service
 public class JwtTokenProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JwtTokenProvider.class);
+
     @Value("${app.jwtSecret}")
     private String jwtSecret;
 
@@ -41,7 +43,7 @@ public class JwtTokenProvider {
         return extractExpiration(token).before(new Date());
     }
 
-    public String createToken(UserDetails userDetails) {
+    public String createToken(UserPrincipal userDetails) {
         Map<String, Object> claims = new HashMap<>();
 
         Date now = new Date();
@@ -56,8 +58,21 @@ public class JwtTokenProvider {
                 .compact();
     }
 
-    public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    public Boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            LOG.error("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            LOG.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            LOG.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            LOG.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            LOG.error("JWT claims string is empty.");
+        }
+        return false;
     }
 }
